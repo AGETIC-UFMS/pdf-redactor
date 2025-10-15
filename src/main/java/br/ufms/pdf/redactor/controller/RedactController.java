d907c5b0eb1c57e565031c7b9add9410a70a7fa9
AGETIC-UFMS <agetic@ufms.br>
2025-10-15 10:35:25 -0400

    add license disclaimer for each file

--- src/main/java/br/ufms/pdf/redactor/controller/RedactController.java
+++ src/main/java/br/ufms/pdf/redactor/controller/RedactController.java
@@ -1,3 +1,23 @@
+/*
+    Copyright (C) 2025 Federal University of Mato Grosso do Sul
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Third-Party Dependencies
+    
+    - iText-pdfSweep-Java - AGPLv3 - Source code at <https://github.com/itext/itext-pdfsweep-java>
+ */
 package br.ufms.pdf.redactor.controller;
 
 import br.ufms.pdf.redactor.model.ManualRedactPdfRequest;
@@ -30,7 +50,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1")
 @Slf4j
 public class RedactController {
 
